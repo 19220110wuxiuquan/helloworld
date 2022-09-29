b9353c594bb2edf675f15c352a2f746d8141b29d
19220110wuxiuquan <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-09-29 20:27:05 +0800

    Create 单位转换.c

---
+++ 单位转换.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define FEET_TO_CM 30.48
+#define INCH_TO_CM 2.54
+int main(int argc, char* argv[])
+{
+    int feet;
+    float cm, inches;
+    printf("CONVERT CM TO INCHES!\n");
+    printf("Enter the height in centimeters :");
+    scanf("%f", &cm);
+    while(cm > 0) {
+        feet = cm / FEET_TO_CM;
+        inches = (cm - feet * FEET_TO_CM) / INCH_TO_CM;
+        printf("%.1f cm = %d feet , %.1f inches\n", cm, feet, inches);
+        printf("Enter the height in centimeters( <=0 TO QUIT ):");
+        scanf("%f", &cm);
+    }
+    printf("PROGRAM EXIT!\n");
+    return 0;
+}
