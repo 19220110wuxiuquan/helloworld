4393ac704e68968a159a044b3f5b646062ffe3b3
19220110wuxiuquan <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-11-17 13:45:31 +0800

    Create 数的倒序.c

---
+++ 数的倒序.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	int digit; 
+	int ret=0;
+	while(x>0)
+	{
+		digit = x%10;
+		printf("%d\n",digit);
+		ret=ret*10+digit;
+		printf("x=%d,digit=%d,ret=%d\n",x,digit,ret);
+		x/=10;
+	}
+	return 0;
+ } 
