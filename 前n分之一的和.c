c9909df8f267405c889c1c7d7955265cd25b5796
19220110wuxiuquan <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-11-17 15:22:17 +0800

    Update 前n分之一的和.c

--- 前n分之一的和.c
+++ 前n分之一的和.c
@@ -5,9 +5,10 @@ int main()
 	scanf("%d",&n);
 	int i;
 	double sum=0.0;
+	//double sign=1.0;
 	for(i=1;i<=n;i++)
 	{
-	     sum+=1.0/i;
+	     sum+=1.0/i;//改为sign/i;sign=-sign;
 	}
 	printf("f(%d)=%f\n",n,sum);
 	return 0;	
