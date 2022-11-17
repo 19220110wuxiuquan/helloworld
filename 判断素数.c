9ba64ea67c99cc92ff1d470ba9c7d8e8bde47109
19220110wuxiuquan <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-11-17 14:31:08 +0800

    Create 判断素数.c

---
+++ 判断素数.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	int i;
+	int isPrime=1;
+	for(i=2;i<x;i++)
+	{
+		if(x%i==0)
+		{
+			isPrime=0;
+			break; 
+		}
+	}
+	if(isPrime==1)
+		{
+			printf("是素数"); 
+		}
+	else
+		{
+			printf("不是素数");
+		} 
+
+	return 0;
+}
