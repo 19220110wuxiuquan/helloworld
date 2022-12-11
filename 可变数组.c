771739667f00ae3cd0b939cd71976fd12e4c984a
wxq <114474518+19220110wuxiuquan@users.noreply.github.com>
2022-12-11 12:22:32 +0800

    Create 可变数组.c

---
+++ 可变数组.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"array.h"
+typedef struct{
+	int *array;
+	int size;
+}Array;
+const BLOCK_SIZE =20;
+Array array_create(int init_size)
+{
+    Array a;
+    a.size=init_size;
+    a.array=(int*)malloc(sizeof(int*)a.size)；
+    return a;
+}
+void array_free(Array*a)
+{
+    free(a->array);
+    a->array=NULL;
+    a->size=0;
+}//封装
+int array_size(const Array*a)
+{
+    return a->size;
+}
+int*array_at(Array*a,int index)
+{
+    if(index>=a->size)
+    {
+        array_inflate(a,(index/BLOCK_SIZE+1)*BLOCK_SIZE-a->size);
+    }
+    return &(a->array[index]);
+}
+void array_inflate(Array*a,int more_size)
+{
+    int *p=(int*)malloc(sizeof(int)(a->size+more_size));
+    int i;
+    for(i=0;i<a->size;i++)
+    {
+        p[i]=a->array[i];
+    }
+    free(a->array);
+    a->array=p;
+    a->size+=more_size;
+}
+imt main(int argc,char const *argv[])
+{
+    Array a=attay_create(100);
+    printf("%d\n",array_create(100));
+    *array_at(&a,0)=10;
+    printf("%d\n",*array_at(&a,0));
+    int number;
+    int cnt=0;
+    while(number!=-1)
+    {
+        scanf("%d",&number);
+        if(number!=-1)
+        {
+            *array_at(&a,cnt++)=number;
+        }
+    }
+    array_free(&a);
+
+    return 0;
+}
